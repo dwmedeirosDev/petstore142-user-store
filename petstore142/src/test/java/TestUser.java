7837b135f5ba6c595494721320152142656164a2
David Medeiros <davidmedeiros94@hotmail.com>
2025-03-04 19:49:11 -0300

    criado atributos e GET

--- petstore142/src/test/java/TestUser.java
+++ petstore142/src/test/java/TestUser.java
@@ -21,7 +21,15 @@ import org.junit.jupiter.api.Test;
 public class TestUser {
     static String ct = "application/json";
     static String baseURL = "https://petstore.swagger.io/v2";
-    static int id = 11865005;
+
+    static int id = 118650010;
+    static int userStatus = 1;
+    static String username = "dwmedeiros";
+    static String firstName = "David";
+    static String lastName = "Medeiros";
+    static String email = "teste@qa.com";
+    static String password = "@Teste123";
+    static String phone = "21999998888";
 
     // Leitura do JSON
     public static String readFileJson(String fileJson) throws IOException {
@@ -34,7 +42,7 @@ public class TestUser {
     // POST
     public void testPostUser() throws IOException {
 
-        // Carregamento dos dados do arquivo JSON do arquivo user1.json
+        // Guardando a leitura do JSON em uma variável
         String jsonBody = readFileJson("src/test/resources/json/user1.json");
 
         given() // Dado que
@@ -54,5 +62,36 @@ public class TestUser {
                 .body("code", is(200))
                 .body("type", is("unknown"))
                 .body("message", is("11865005"));
+    }
+
+    @Test
+    @Order(2)
+
+    // GET
+    public void testGetUser() {
+        given() // Dado que
+                .contentType(ct)
+                .log().all()
+
+                .when() // Quando
+                .get(baseURL + "/user/" + username)
+
+                .then() // Então
+                .log().all()
+
+                // Comparando o código de requisição
+                .statusCode(200)
+
+                // Comparando as respostas (Eco)
+                .body("id", is(id))
+                .body("username", is(username))
+                .body("firstName", is(firstName))
+                .body("lastName", is(lastName))
+                .body("email", is(email))
+                .body("password", is(password))
+                .body("phone", is(phone))
+                .body("userStatus", is(userStatus))
+
+        ;
     }
 }
