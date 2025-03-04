492da337fcd76c12d4dd6c8660254dd0a1ca0af6
David Medeiros <davidmedeiros94@hotmail.com>
2025-03-04 19:32:10 -0300

    tratamento de erro e post

--- petstore142/src/test/java/TestUser.java
+++ petstore142/src/test/java/TestUser.java
@@ -1,3 +1,10 @@
+
+// Comparadores do Hamcrest
+import static org.hamcrest.Matchers.is;
+
+// Função given
+import static io.restassured.RestAssured.given;
+
 // Tratamento de erro
 import java.io.IOException;
 
@@ -5,13 +12,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+// Ordem dos @Test
+import org.junit.jupiter.api.Order;
+
 // Import do @Test
 import org.junit.jupiter.api.Test;
 
 public class TestUser {
-    static String ct = "aplication/json";
-    static String uriPet = "petstore.swagger.io/v2/user";
-    static String petId = "118650010"; // Número fixo sem necessidade de criar em forma de "int"
+    static String ct = "application/json";
+    static String baseURL = "https://petstore.swagger.io/v2";
+    static int id = 11865005;
 
     // Leitura do JSON
     public static String readFileJson(String fileJson) throws IOException {
@@ -19,9 +29,30 @@ public class TestUser {
     }
 
     @Test
+    @Order(1)
 
     // POST
-    public void testPostUser() {
+    public void testPostUser() throws IOException {
 
+        // Carregamento dos dados do arquivo JSON do arquivo user1.json
+        String jsonBody = readFileJson("src/test/resources/json/user1.json");
+
+        given() // Dado que
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+                .when() // Quando
+                .post(baseURL + "/" + "user")
+
+                .then() // Então
+                .log().all()
+
+                // Comparando o código de requisição
+                .statusCode(200)
+
+                // Comparando as respostas
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is("11865005"));
     }
 }
