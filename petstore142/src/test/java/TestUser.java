ca41db0a0c773f18e7e88a7d662e3032dda43e6a
David Medeiros <davidmedeiros94@hotmail.com>
2025-03-05 10:25:06 -0300

    teste de contrato na resposta eco do GET, criado a arquivo JSON user2.json e escrito código PUT

--- petstore142/src/test/java/TestUser.java
+++ petstore142/src/test/java/TestUser.java
@@ -1,5 +1,7 @@
 
 // Comparadores do Hamcrest
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 
 // Função given
@@ -22,7 +24,7 @@ public class TestUser {
     static String ct = "application/json";
     static String baseURL = "https://petstore.swagger.io/v2";
 
-    static int id = 118650010;
+    static int id = 11865005;
     static int userStatus = 1;
     static String username = "dwmedeiros";
     static String firstName = "David";
@@ -50,7 +52,7 @@ public class TestUser {
                 .log().all()
                 .body(jsonBody)
                 .when() // Quando
-                .post(baseURL + "/" + "user")
+                .post(baseURL + "/user")
 
                 .then() // Então
                 .log().all()
@@ -92,6 +94,56 @@ public class TestUser {
                 .body("phone", is(phone))
                 .body("userStatus", is(userStatus))
 
+                // Teste de contrato
+
+                // Verificação das propriedadas existentes na resposta
+                .body("$", hasKey("id"))
+                .body("$", hasKey("username"))
+                .body("$", hasKey("firstName"))
+                .body("$", hasKey("lastName"))
+                .body("$", hasKey("email"))
+                .body("$", hasKey("password"))
+                .body("$", hasKey("phone"))
+                .body("$", hasKey("userStatus"))
+
+                // Verificação dos formatos das propriedades existentes na resposta
+                .body("id", instanceOf(Integer.class))
+                .body("username", instanceOf(String.class))
+                .body("firstName", instanceOf(String.class))
+                .body("lastName", instanceOf(String.class))
+                .body("email", instanceOf(String.class))
+                .body("password", instanceOf(String.class))
+                .body("phone", instanceOf(String.class))
+                .body("userStatus", instanceOf(Integer.class))               
         ;
+    }
+
+    @Test
+    @Order(3)
+
+    // PUT
+    public void testPutUser() throws IOException {
+
+        // Guardando a leitura do JSON em uma variável
+        String jsonBody = readFileJson("src/test/resources/json/user2.json");
+
+        given() // Dado que
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+
+                .when() // Quando
+                .put(baseURL + "/user/" + username)
+
+                .then() // Então
+                .log().all()
+
+                // Comparando o código de requisição
+                .statusCode(200)
+
+                // Comparando as respostas
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is("11865005"));
     }
 }
